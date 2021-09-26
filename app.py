73646cdfad40e8a9373513fb0431b35d7a1d8ea8
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-09-26 00:25:15 -0400

    add database design

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.odd_number
+collection = db.number_pool
 
 @app.route('/')
 def index():
   return '<h1>Website under construction. Please visit later.</h1>'
+
+@app.route('/search/<number>')
+def search(number):
+  pass
+
+
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def main():
+  current = datetime.now()
+  print(type(str(current)))
+
+
+if __name__ == '__main__':
+  main()
