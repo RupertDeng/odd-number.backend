5f880f5a1ee3af9e52272eed6da22785d1c8e7cb
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-07 16:35:31 -0400

    implemented add-message endpoint

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from pymongo import MongoClient
+from pymongo.collection import ReturnDocument
+from datetime import datetime
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -15,17 +17,39 @@ def index():
 
 @app.route('/search/<number>')
 def search(number):
-  number_pool.update_one(
+  record = number_pool.find_one_and_update(
     {'number': number},
     {'$inc': {'searched': 1}},
-    upsert=True
+    upsert=True,
+    return_document=ReturnDocument.AFTER
     )
-  record = number_pool.find_one({'number': number})
   response = {
     'searched': record['searched'],
     'messages': record['messages'] if 'messages' in record else [],
   }
   return jsonify({'response': response})
+
+
+@app.route('/add-message/<number>', methods=['POST'])
+def add_message(number):
+  data = request.get_json()
+  time_id = str(datetime.now())
+  new_message = {
+      'time_id': time_id,
+      'tag': data['tag'],
+      'text': data['text'],
+      'upvote': 0,
+      'downvote': 0
+    }
+  number_pool.update_one(
+    {'number': number},
+    {'$push': {'messages': new_message}},
+    upsert=True
+  )
+  response = make_response('message added', 201)
+  return response
+
+
 
 
 
