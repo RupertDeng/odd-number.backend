36d1428abd83a519ce492cb6928a76f3d91e40a3
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-07 14:15:58 -0400

    add search number endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,15 +7,33 @@ db = client.odd_number
 number_pool = db.number_pool
 ip_pool = db.ip_pool
 
+
 @app.route('/')
 def index():
   return '<h1>Website under construction. Please visit later.</h1>'
 
+
 @app.route('/search/<number>')
 def search(number):
-  
+  number_pool.update_one(
+    {'number': number},
+    {'$inc': {'searched': 1}},
+    upsert=True
+    )
+  record = number_pool.find_one({'number': number})
+  response = {
+    'searched': record['searched'],
+    'messages': record['messages'] if 'messages' in record else [],
+  }
+  return jsonify({'response': response})
 
 
+
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
 
 
 
