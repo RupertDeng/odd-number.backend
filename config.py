d9cc26d8f9039b1a5c4873f7b64cb9f45272daef
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-15 11:23:44 -0400

    add api key control

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import environ
-import local_config
+# import local_config
 
-# local keys for development
-# DB_CONN_STR = local_config.DB_CONN_STR_ATLAS
-DB_CONN_STR = local_config.DB_CONN_STR_LOCAL
-APP_API_KEY = local_config.API_KEY_LOCAL
+# # local keys for development
+# # DB_CONN_STR = local_config.DB_CONN_STR_ATLAS
+# DB_CONN_STR = local_config.DB_CONN_STR_LOCAL
+# APP_API_KEY = local_config.API_KEY_LOCAL
 
 # remote keys from backend server environment variables
-# DB_CONN_STR = environ.get('DB_CONN_STR')
-# APP_API_KEY = environ.get('APP_API_KEY')
+DB_CONN_STR = environ.get('DB_CONN_STR')
+APP_API_KEY = environ.get('APP_API_KEY')
