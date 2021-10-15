0a6a76c8b26557238f6d1aa9f33cd367ec827ca3
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-14 23:37:19 -0400

    temp transit to local development

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from os import environ
 import local_config
 
 # local keys for development
-DB_CONN_STR = local_config.DB_CONN_STR_ATLAS
+# DB_CONN_STR = local_config.DB_CONN_STR_ATLAS
+DB_CONN_STR = local_config.DB_CONN_STR_LOCAL
 APP_API_KEY = local_config.API_KEY_LOCAL
 
 # remote keys from backend server environment variables
